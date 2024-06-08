559090ea97fb2bde2251fb0d6a527abda198a5fd
Sushmitha Sj <Sushmitha.Sj@amadeus.com>
2024-06-08 10:44:07 +0530

    my first commit

--- PythonLearning/Hello world.py
+++ PythonLearning/Hello world.py
@@ -1,4 +1,19 @@
 print("Hello world")
 
-print(2/2)
-print(2%2)
+print(2 / 2)
+print(2 % 2)
+
+# self - used in concept of opps
+
+# *args - Unlimited number of arguments
+
+print("Hello", "World", "true", "123", "3.14")
+# sep - how you want to separate arguments
+print("Hello", "World", "true", "123", "3.14", sep="-")
+# end - how you want to end the line
+print("Hello", "World", "true", "123", "3.14", end=" ")
+print("Hello", "World", "true", "123", "3.14", sep="-", end=" ")
+
+print("I am a good person", end="_")
+print("I am a bad person", end="_")
+#
