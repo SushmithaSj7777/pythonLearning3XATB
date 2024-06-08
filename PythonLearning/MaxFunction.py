431eb214d61cda4a30abc7df292111dea0285f70
Sushmitha Sj <Sushmitha.Sj@amadeus.com>
2024-06-08 11:06:01 +0530

    Maxfunction

---
+++ PythonLearning/MaxFunction.py
@@ -0,0 +1,5 @@
+print(max(1, 22, 100, -1, 0, 77))
+
+# type error when we add string in arguments
+# TypeError: '>' not supported between instances of 'str' and 'int'
+print(max(1, 0, 7, "sush"))
---
+++ PythonLearning/Print function.py
@@ -0,0 +1,14 @@
+# self - used in concept of opps
+
+# *args - Unlimited number of arguments
+
+print("Hello", "World", "true", "123", "3.14")
+# sep - how you want to separate arguments
+print("Hello", "World", "true", "123", "3.14", sep="-")
+# end - how you want to end the line
+print("Hello", "World", "true", "123", "3.14", end=" ")
+print("Hello", "World", "true", "123", "3.14", sep="-", end=" ")
+
+print("I am a good person", end="_")
+print("I am a bad person", end="_")
+#
